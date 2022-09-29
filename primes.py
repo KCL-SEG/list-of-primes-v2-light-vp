ae76a1e0dba4b8831627285b8c38e784be06c9b9
Vaibhavkumar Patel <Vaibhavkumar.patel@kcl.ac.uk>
2022-09-29 08:38:20 +0100

    Rasie Value error

--- primes.py
+++ primes.py
@@ -3,4 +3,20 @@
 
 def primes(number_of_primes):
     list = []
+    if number_of_primes < 1:
+        raise ValueError
+    else:
+        list.append(2)
+        x = 3
+        while len(list) < number_of_primes:
+            if isprime(x):
+                list.append(x)
+                x += 1
     return list
+
+def isprime(number):
+    for i in range(2, int(number/2)+1):
+        if(number % i) == 0:
+            return False
+    else:
+        return True
